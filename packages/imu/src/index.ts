02646e97fd8b5059181a4fcc0b47fb0b4e461f19
Daniel Nyong <danielnyong66@gmail.com>
2025-12-29 08:49:55 +0100

    Added backfill command

--- packages/imu/src/index.ts
+++ packages/imu/src/index.ts
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import { rollbackCommand } from './commands/rollback';
+import { Command } from "commander";
+import { rollbackCommand } from "./commands/rollback";
+import { backfillCommand } from "./commands/backfill";
 
 const program = new Command();
 
 program
-  .name('imu')
-  .description('Indexer Management Utility - Maintenance tools for the Custom Ethereum Indexer')
-  .version('1.0.0');
+  .name("imu")
+  .description(
+    "Indexer Management Utility - Maintenance tools for the Custom Ethereum Indexer",
+  )
+  .version("1.0.0");
 
 program.addCommand(rollbackCommand);
+program.addCommand(backfillCommand);
 
 program.parse(process.argv);
